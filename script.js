e5f6ba5f2e32d32365bf0b88e7e4633f1c0d773c
Peter Weigman <phweigman@gmail.com>
2021-06-19 14:39:19 -0400

    improved comments to clarify logic and corrected logic from changing how user input is accepted (was prompt, now is confirm)

--- script.js
+++ script.js
@@ -43,84 +43,83 @@ function generatePassword() {
   let userLCaseInput = confirm('Would you like lowercase characters in your password? Click OK for yes or Cancel for no.');
 
 
-/*UPPER CASE CHARACTER CHECK*/
-    //check if user wants uppercase characters in password
-    let userUCaseInput = confirm('Would you like uppercase characters in your password? Click OK for yes or Cancel for no.');
+  /*UPPER CASE CHARACTER CHECK*/
+  //check if user wants uppercase characters in password
+  let userUCaseInput = confirm('Would you like uppercase characters in your password? Click OK for yes or Cancel for no.');
 
 
-/*NUMERIC CHARACTER CHECK*/
-      //check if user wants numbers in password
+  /*NUMERIC CHARACTER CHECK*/
+  //check if user wants numbers in password
   let userNumericInput = confirm('Would you like numeric characters in your password? Click OK for yes or Cancel for no.');
 
 
 
   /*SPECIAL CHARACTER CHECK*/
-    //check if user wants special characters in password
-    let userSpecCharInput = confirm('Would you like special characters in your password? Click OK for yes or Cancel for no.');
+  //check if user wants special characters in password
+  let userSpecCharInput = confirm('Would you like special characters in your password? Click OK for yes or Cancel for no.');
 
 
 
-    if (!userLCaseInput && !userUCaseInput && !userNumericInput && !userSpecCharInput){
-      alert('You must choose at least one type of character. Please start over.');
-      return '';
-    }
-    
-    //first collect all characters to be used in password. This is
-    // to ensure all desired characters are used at least once
+  if (!userLCaseInput && !userUCaseInput && !userNumericInput && !userSpecCharInput) {
+    alert('You must choose at least one type of character. Please start over.');
+    return '';
+  }
 
-    //also add chars to array of possibleChars if they are usable in password
- let passwordArray = new Array();
- let possibleChars = new Array();
- let randNum = 0;
 
- //check if lowercase is to be used. If so, pick one to ensure at least one is in password
- if (pwLCase) {
-   randNum = Math.floor(Math.random()*(lowerCaseChars.length));
-   passwordArray.push(lowerCaseChars[randNum]);
-   possibleChars.push(lowerCaseChars);
- }
+  //create arrays to store password and an array to store the possible characters for the password
+  let passwordArray = new Array();
+  let possibleChars = new Array();
+  let randNum = 0;
 
- //check if uppercasae is to be used. If so, pick one to ensure at least one is in password
- if (pwUCase) {
-  randNum = Math.floor(Math.random()*(upperCaseChars.length));
-  passwordArray.push(upperCaseChars[randNum]);
-  possibleChars.push(upperCaseChars);
-}
+  //check if lowercase is to be used. If so, pick one to ensure at least one is in password
+  if (userLCaseInput) {
+    randNum = Math.floor(Math.random() * (lowerCaseChars.length));
+    passwordArray.push(lowerCaseChars[randNum]);
+    possibleChars.push(lowerCaseChars);
+  }
 
-//check if numeric is to be used. If so, pick one to ensure at least one is in password
-if (pwNumeric) {
-  randNum = Math.floor(Math.random()*(digits.length));
-  passwordArray.push(digits[randNum]);
-  possibleChars.push(digits);
-}
+  //check if uppercasae is to be used. If so, pick one to ensure at least one is in password
+  if (userUCaseInput) {
+    randNum = Math.floor(Math.random() * (upperCaseChars.length));
+    passwordArray.push(upperCaseChars[randNum]);
+    possibleChars.push(upperCaseChars);
+  }
 
-//check if special chars are to be used. If so, pick one to ensure at least one is in password
-if (pwSpecChar) {
-  randNum = Math.floor(Math.random()*(specChars.length));
-  passwordArray.push(specChars[randNum]);
-  possibleChars.push(specChars);
-}
+  //check if numeric is to be used. If so, pick one to ensure at least one is in password
+  if (userNumericInput) {
+    randNum = Math.floor(Math.random() * (digits.length));
+    passwordArray.push(digits[randNum]);
+    possibleChars.push(digits);
+  }
 
-//figure out how many chars to get
-const numCharsToPick = pwLen-passwordArray.length;
-//Fill out remainder of password Array
-for (let i = 0; i <numCharsToPick; i++){
-  //choose type of char first with random number
-  randNum = Math.floor(Math.random()*possibleChars.length);
-  //choose char with a different random number
-  let innerRandNum = Math.floor(Math.random()*(possibleChars[randNum].length));
-  //add new char to password array
-  passwordArray.push(possibleChars[randNum][innerRandNum]);
-}
+  //check if special chars are to be used. If so, pick one to ensure at least one is in password
+  if (userSpecCharInput) {
+    randNum = Math.floor(Math.random() * (specChars.length));
+    passwordArray.push(specChars[randNum]);
+    possibleChars.push(specChars);
+  }
 
-//finally, randomize password order using possible characters
-let password = '';
-for (let i = 0; i<pwLen; i++){
-  randNum = Math.floor(Math.random()*passwordArray.length);
-  password+=passwordArray[randNum];
-  passwordArray.splice(randNum,1);
-}
-return password;
+
+  //figure out how many chars to get
+  const numCharsToPick = pwLen - passwordArray.length;
+  //Fill out remainder of password Array
+  for (let i = 0; i < numCharsToPick; i++) {
+    //choose type of char first with random number
+    randNum = Math.floor(Math.random() * possibleChars.length);
+    //choose specific char with a different random number
+    let innerRandNum = Math.floor(Math.random() * (possibleChars[randNum].length));
+    //add new char to password array
+    passwordArray.push(possibleChars[randNum][innerRandNum]);
+  }
+
+  //finally, randomize password order using possible characters
+  let password = '';
+  for (let i = 0; i < pwLen; i++) {
+    randNum = Math.floor(Math.random() * passwordArray.length);
+    password += passwordArray[randNum];
+    passwordArray.splice(randNum, 1);
+  }
+  return password;
 }
 
 // Add event listener to generate button
